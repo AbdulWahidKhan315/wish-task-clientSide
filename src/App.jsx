98efbec11df420c74ab3311f8bf477a72683532a
AbdulWahidKhan315 <k.wahidkhan55@gmail.com>
2023-12-21 16:20:34 +0600

    added banner for home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import banner from '../src/assets/taskBanner.jpg'
+import { Link } from 'react-router-dom'
 
 function App() {
 
   return (
     <div>
       <Navbar></Navbar>
+      <div className='container mx-auto relative'>
+        <img className='lg:h-[80vh] w-full object-cover rounded-t-3xl' src={banner} alt="" />
+        <p className='text-2xl md:text-5xl font-bold font-serif absolute top-10 text-fuchsia-500'>Simplify Your Life with Wish Task</p>
+        <Link to='/login'>
+          <button className='btn bg-purple-700 hover:bg-purple-900 text-white text-2xl absolute top-32 left-10 border-2 border-purple-600 shadow-2xl shadow-red-500'>Lets Explore</button>
+        </Link>
+      </div>
     </div>
   )
 }
---
+++ src/components/TypesOfPeople.jsx
@@ -0,0 +1,10 @@
+
+const TypesOfPeople = () => {
+    return (
+        <div>
+            <h1>..People Are Using..</h1>
+        </div>
+    );
+};
+
+export default TypesOfPeople;
